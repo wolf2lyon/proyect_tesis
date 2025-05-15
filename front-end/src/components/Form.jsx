641a727bf8970e0414551a2b1eea2057af349826
wolf2lyon <josesebastiancu@gmail.com>
2025-05-14 23:09:41 -0500

    change url

--- front-end/src/components/Form.jsx
+++ front-end/src/components/Form.jsx
@@ -30,7 +30,7 @@ export const Form = ({ children, files, jobDescription, setUserResponse,userResp
     }
 
     try {
-      const response = await fetch("http://localhost:5000/matcher", {
+      const response = await fetch("http://127.0.0.1:5000/matcher", {
         method: "POST",
         body: formData,
       });
